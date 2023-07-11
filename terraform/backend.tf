f60ec4f0f1343dad986abdd8544bf0db84a5c8d3
kishoremcc <kishore_mcc@yahoo.com>
2023-07-12 01:10:30 +0530

    first commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
+    bucket = "emailos-app"
     region = "us-east-1"
-    key = "eks/terraform.tfstate"
+    key = "rednew/terraform.tfstate"
   }
 }
